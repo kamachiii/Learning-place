85f87657541baec3208af3a754b376a7c9b6cf5c
kamachiii <hengkert3jy@gmail.com>
2022-11-02 13:10:32 +0700

    tambah operator

--- JAVA/helloworld.java
+++ JAVA/helloworld.java
@@ -1,5 +1,7 @@
-public class helloworld{
+public class helloworld
+/*nama untuk class harus sesuai dengan nama file*/{
     public static void main(String[] args){
+        /* menampilkan pesan di terminal */
         System.out.println("Hello World!");
     }
 }
---
+++ JAVA/operator.java
@@ -0,0 +1,51 @@
+public class operator {
+    public static void main(String[] args) {
+    /* operator ada 6 yaitu :
+    1. operator aritmatika
+     * z = x + y
+     * z = x - y
+     * z = x * y
+     * z = x / y
+     * z = x % y
+     * z = x ** y
+    */
+    System.out.format("%d + %d = %d",2,3,2+3);
+    System.out.format("%d + %d = %d",2,3,2+3);
+    System.out.format("%d + %d = %d",2,3,2+3);
+    System.out.format("%d + %d = %d",2,3,2+3);
+    System.out.format("%d + %d = %d",2,3,2+3);
+    System.out.format("Dafa Mahandika is here");
+    /*
+    2. operator penugasan
+     * y = x
+     * y += x
+     * y -= x
+     * y *= x
+     * y %= x
+    3. Operator pembagi
+     * x > y = x lebih besar daripada y
+     * x < y = x lebih kecil daripada y
+     * x >= y = x lebih besar sama dengan y
+     * x <= y = x lebih kecil sama dengan y
+     * x == y = x sama dengan y
+     * x != y = x tidak sama dengan y
+    4. operator logika
+     * && = logika dan / and
+     * || = logika atau / or
+     * ! = logika kebalikan
+    5. operator bitwise (hanya untuk type int, long, short, char, dan byte)
+     * & = dan / and
+     * | = atau / or
+     * ^ = xor
+     * ~ = negasi/kebalikan
+     * << = left shift
+     * >> = right shift
+     * <<< = left shift (unsigned)
+     * >>> = right shift (unsigned)
+    6. operator ternary
+     * kamu ? "benar" : "salah";
+    */
+
+    }
+
+}
