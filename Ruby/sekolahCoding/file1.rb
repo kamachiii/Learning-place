42c6339b7dc4d02c7e0ef4f183df7d8c53050f7b
kamachiii <hengkert3jy@gmail.com>
2022-11-23 12:56:54 +0700

    input gets.chomp

--- Ruby/sekolahCoding/file1.rb
+++ Ruby/sekolahCoding/file1.rb
@@ -1,40 +1,64 @@
 #*ini  komentar dalam ruby -> variabel
 #*tipeData -> string, number, array, hash
+##* Tipe String
 text = "Halo semuanya!"
 text2 = "...apa kabar?"
 nama1 = " Kamil"
 nama2 = " Kamachi"
 puts text+text2+nama2
 
+
+##* tipe integer
 a = "20" #*string
 b = "30" #*string
 c = 20.1 #*integer
 d = 30.123 #*integer
 msg = "gitu loh!"
 puts a+b #*output = 2030
-puts c+d #*output = 50
+puts c+d #*output = 50.223
 
 # puts "Hasil dari c + d adalah "+c+d #!error
 puts "Hasil dari c + d adalah #{c+d} #{msg}\n" #*work
 
-#* ini komentar dalam ruby
-#* variabel || tipeData -> string, number, array, hash
 
+#*Perbedaan puts dan print
 text = "Hallo "
 tuxt = "HeyHey "
-
+#! menampilkan dalam line yang berbeda
 # puts text
 # puts tuxt
-#! menampilkan dalam line yang berbeda
 
+#* menampilkan dalam line yang sama
 print text
 print tuxt
-#* menampilkan dalam line yang sama
-
+#* contoh lain
 h = "h"
 a = "a"
 i = "i"
-
 print h
 print a
 print i
+
+#*memberi line baru
+puts ""
+
+
+#* input dan get.chomp
+##* input dan get.chomp String
+puts "Umurnya berapa ngabs? "
+umur = gets.chomp
+puts "Kakinya berapa ngabs? "
+kaki = gets.chomp
+puts "jadi umur loe #{umur} dan kaki loe #{kaki}"
+
+##* input dan get.chomp integer
+puts "Masukkan angka1"
+a = gets.chomp
+puts "Masukkan angka2"
+b = gets.chomp
+
+#!gagal karena dianggap string
+puts a+b
+
+#*berhasil karena dianggap integer
+puts a.to_i+b.to_i
