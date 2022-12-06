1d856092472c4b8f0b508de9b1f481200100d88e
kamachiii <hengkert3jy@gmail.com>
2022-12-06 21:47:49 +0700

    tambah C | pernyataan if-else-tenary

--- C/pernyataanIf.c
+++ C/pernyataanIf.c
@@ -9,13 +9,41 @@ int main(){
 		"perintah";
 	}
 */
-	int x;
-	x = 10;
-	if(x %2 == 0)
-		printf("bilangan genap\n");
+int x;
+x = 5;
+if(x % 2 == 0)
+	printf("bilangan genap\n");
 		
-	if(x %2 == 1)
-		printf("bilangan ganjil\n");
+if(x % 2 == 1)
+	printf("bilangan ganjil\n");
 		
-	return 0;
+/* menggunakan if-else */
+int y = 10;
+if(y % 2 == 0){
+	printf("bilangan genap\n");
+}else{
+	printf("bilangan ganjil\n");
+}
+
+/* menggunakan elseif */
+int nilai = 80;
+if(nilai >= 90){
+	printf("Nilai huruf : A\n");
+}else if(nilai >=75){
+	printf("Nilai huruf : B\n");
+}else if(nilai >=60){
+	printf("Nilai huruf : C\n");
+}else if(nilai >=40){
+	printf("Nilai huruf : D\n");
+}else {
+	printf("Nilai huruf : E\n");
+}
+printf("Dengan perolehan nilai : %d\n",nilai);
+
+/* menggunakan tenary */
+int z;
+z = 25;
+printf("%s", ((z % 2 == 0) ? ("bilangan genap") : ("bilangan ganjil")));
+
+return 0;
 }
